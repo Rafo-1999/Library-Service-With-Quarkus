e02c3ec843c89252608753a9d037fe0ec2f5dd8b
rafik.mnatsakanyan <rafik.mnatsakanyan@polixis.com>
2025-02-03 13:31:54 +0400

    build native

--- src/test/java/org/library/NativeBookResourceIT.java
+++ src/test/java/org/library/NativeBookResourceIT.java
@@ -1,8 +1,26 @@
 package org.library;
 
-import io.quarkus.test.junit.NativeImageTest;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.is;
 
-@NativeImageTest
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import org.junit.jupiter.api.Test;
+
+@QuarkusIntegrationTest
 public class NativeBookResourceIT extends BookResourceTest{
 
+    @Test
+    public  void shouldGetBookById() {
+        given()
+            .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN)
+            .pathParam("id", 1);
+        when()
+            .get("api/books/1")
+            .then().statusCode(200).body("author", is("Joshua Bloch"),
+                                         "title",is("Effective Java"),
+                                         "genre",is("Prod-IT"));
+    }
 }
